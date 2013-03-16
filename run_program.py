a16bde31ddf27e541e91fae2ff697611da1bb470
marianitadn <maria.nita.dn@gmail.com>
2013-03-16 18:18:43 +0200

    read from cli data types

--- run_program.py
+++ run_program.py
@@ -1,17 +1,26 @@
-import cli
+import cli.app
 
 import binary_heap as heap
-from test_data_generator import *
+from test_data_generator import TEST_DATA
 
+@cli.app.CommandLineApp
+def read_from_cli(app):
+    params = {
+        'DATA_TYPE': app.params.data,
+        'VALUE_TYPE': app.params.value}
 
-def read_from_cli():
-    run()
+    run_program(**params)
+    
+read_from_cli.add_param('--data', help='', type=str, default='BOUNDARY_VALUES')
+read_from_cli.add_param('--value', help='', type=str, default='NULL')
 
-def run():
-    null_heap = heap.create_heap(BOUNDARY_VALUES['NULL'])
+def run_program(**params):
+    print "Running algorithm with data: "
+    print TEST_DATA[params['DATA_TYPE']][params['VALUE_TYPE']]
+
+    null_heap = heap.create_heap(
+        TEST_DATA[params['DATA_TYPE']][params['VALUE_TYPE']])
     heap.extract_from(null_heap)
 
 if __name__ == '__main__':
-    read_from_cli()
-
-
+    read_from_cli.run()
