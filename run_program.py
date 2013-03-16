3339ed6f694a5eed5941ad1cf6ac023239a0edf0
marianitadn <maria.nita.dn@gmail.com>
2013-03-16 18:33:19 +0200

    added env option, to run more types of programs

--- run_program.py
+++ run_program.py
@@ -3,23 +3,48 @@ import cli.app
 import binary_heap as heap
 from test_data_generator import TEST_DATA
 
+ENV_HELP = ("Choose in what environment to run the program. Options:" +
+           "     new = create a new heap and extract heap." + 
+           "     example = run the example.")
+
 @cli.app.CommandLineApp
 def read_from_cli(app):
     params = {
         'DATA_TYPE': app.params.data,
-        'VALUE_TYPE': app.params.value}
-
+        'VALUE_TYPE': app.params.value,
+        'ENV': app.params.env}
+    
     run_program(**params)
     
-read_from_cli.add_param('--data', help='', type=str, default='BOUNDARY_VALUES')
-read_from_cli.add_param('--value', help='', type=str, default='NULL')
+read_from_cli.add_param('--data', help='General data partitioning type.',
+                        type=str, default='BOUNDARY_VALUES')
+
+read_from_cli.add_param('--value',
+                        help='A list of data available for a partitioning type',
+                        type=str, default='NULL')
+
+read_from_cli.add_param('--env', help=ENV_HELP, type=str, default='new')
 
 def run_program(**params):
-    print "Running algorithm with data: "
-    print TEST_DATA[params['DATA_TYPE']][params['VALUE_TYPE']]
+    """ Run binary heap algorithm with data from test_data_generator file.
+    Data is chosen based on command line input. Default data: [0, 0].
+    """
 
-    null_heap = heap.create_heap(
-        TEST_DATA[params['DATA_TYPE']][params['VALUE_TYPE']])
+    if params['ENV'] == 'new':
+        new_heap(**params)
+
+    elif params['ENV'] == 'example':
+        heap.example()
+
+def new_heap(**params):
+    data = TEST_DATA[params['DATA_TYPE']][params['VALUE_TYPE']]
+
+    print "Running algorithm with data: "
+    print data
+    
+    # Create a heap.
+    null_heap = heap.create_heap(data)
+    # Extract the heap.
     heap.extract_from(null_heap)
 
 if __name__ == '__main__':
