05a556058bb8ac5e8f4cde832393ab3c1e7cb5ad
marianitadn <maria.nita.dn@gmail.com>
2013-03-16 19:00:13 +0200

    added some values

--- project/test_data_generator.py
+++ project/test_data_generator.py
@@ -5,13 +5,18 @@
 """
 TEST_DATA = {
     # Equivalence partitioning
-    'VALID_PARTITION': {},
+    'VALID_PARTITION': {
+        '': []},
+
     'INVALID_PARTITION': {},
 
     # Boundary values
     'BOUNDARY_VALUES': {
+        'EMPTY': [],
         'NULL': [0, 0],
-        'EMPTY': []},
+        'CONSTANT_POSITIVE': [1, 1, 1],
+        'CONSTANT_NEGATIVE': [-2, -2, -2],
+        },
 
     # Category values
     'CATEGORY_VALUES': {}
