18b48a9b925628022ecb07ca7d4ddd84c460988f
marianitadn <maria.nita.dn@gmail.com>
2013-03-28 20:34:23 +0200

    refactored test

--- project/test_triangle.rb
+++ project/test_triangle.rb
@@ -1,38 +1,32 @@
 require './triangle'
 require './test_data'
 
-=begin
-class TestTriangle < Test::Unit::TestCase
-
-    def setup
-        @tr_scalene = Triangle.new(VALID_PARTITION['scalene'][0])
-    end
-
-    def test_is_triangle
-        assert(@tr_scalene.is_triangle?)
-    end
-end
-=end
-
 describe Triangle do
 
+before(:each) do
+    @triangle_scalene = Triangle.new(VALID_PARTITION['scalene'][0])
+    @triangle_isosceles  = Triangle.new(VALID_PARTITION['isosceles'][0])
+    @triangle_equi = Triangle.new(VALID_PARTITION['equilateral'][0])
+end
+
     describe '#is_triangle?' do
-        subject(:triangle) {Triangle.new(VALID_PARTITION['scalene'][0])}
+        subject(:triangle) {@triangle_scalene}
         it 'should return true' do
             expect(triangle.is_triangle?).to be(true) 
         end
     end
 
     describe '#show' do
-        subject(:triangle) {Triangle.new(VALID_PARTITION['scalene'][0])}
+        subject(:triangle) {@triangle_scalene}
         it 'should show edges' do
             expect(triangle.show).to eq('1, 2, 3')
         end
     end
 
     describe '#type' do
-        subject(:triangle_scalene) {Triangle.new(VALID_PARTITION['scalene'][0])}
-        subject(:triangle_isosceles) {Triangle.new(VALID_PARTITION['isosceles'][0])}
+        subject(:triangle_scalene) {@triangle_scalene}
+        subject(:triangle_isosceles) {@triangle_isosceles}
+
         it 'should return triangle type' do
             expect(triangle_scalene.type).to eq("Scalene")
             expect(triangle_isosceles.type).to eq("Isosceles")
@@ -40,9 +34,10 @@ describe Triangle do
     end
 
     describe '#is_equilateral' do
-        subject(:triangle_scalene) {Triangle.new(VALID_PARTITION['scalene'][0])}
-        subject(:triangle_isosceles) {Triangle.new(VALID_PARTITION['isosceles'][0])}
-        subject(:triangle_equi) {Triangle.new(VALID_PARTITION['equilateral'][0])}
+        subject(:triangle_scalene) {@triangle_scalene}
+        subject(:triangle_isosceles) {@triangle_isosceles}
+        subject(:triangle_equi) {@triangle_equi}
+
         it 'should return true for triangle_equi, otherwhise false' do
             expect(triangle_scalene.is_equilateral?).to be(false)
             expect(triangle_isosceles.is_equilateral?).to be(false)
