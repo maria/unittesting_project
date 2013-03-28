5691a83866eb3d73fc5b23d67fbbdcd114b1e18b
marianitadn <maria.nita.dn@gmail.com>
2013-03-28 19:54:21 +0200

    defined test using rspec and mutated

--- project/test_triangle.rb
+++ project/test_triangle.rb
@@ -1,8 +1,7 @@
-require 'test/unit'
-
 require './triangle'
 require './test_data'
 
+=begin
 class TestTriangle < Test::Unit::TestCase
 
     def setup
@@ -11,5 +10,13 @@ class TestTriangle < Test::Unit::TestCase
 
     def test_is_triangle
         assert(@tr_scalene.is_triangle?)
+    end
+end
+=end
+
+describe Triangle, '#is_triangle?' do
+    subject(:triangle) {Triangle.new(VALID_PARTITION['scalene'][0])}
+    it 'shoudl return true' do
+        expect(triangle.is_triangle?).to be(true) 
     end
 end
