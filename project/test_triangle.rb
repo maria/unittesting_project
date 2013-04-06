a52bbafad2fa2404ba4f8650f00552387cd64a99
marianitadn <maria.nita.dn@gmail.com>
2013-04-06 22:41:11 +0300

    updated subjects and data

--- project/test_triangle.rb
+++ project/test_triangle.rb
@@ -4,56 +4,73 @@ require './data'
 describe Triangle do
 
 before(:each) do
-    @triangle_scalene = Triangle.new(VALID_PARTITION['scalene'][0])
-    @triangle_isosceles  = Triangle.new(VALID_PARTITION['isosceles'][0])
-    @triangle_equi = Triangle.new(VALID_PARTITION['equilateral'][0])
+    @invalid_scalene = Triangle.new(INVALID_PARTITION['scalene'][0])
+    @valid_scalene = Triangle.new(VALID_PARTITION['scalene'])
+    @valid_isosceles  = Triangle.new(VALID_PARTITION['isosceles'])
+    @valid_equilateral = Triangle.new(VALID_PARTITION['equilateral'])
 end
 
     describe '#is_triangle?' do
-        subject(:triangle) {@triangle_scalene}
-        it 'should return true' do
+
+      context "is a triangle" do
+        subject(:triangle) {@valid_scalene}
+          it 'should return true' do
             expect(triangle.is_triangle?).to be(true) 
         end
+      end 
+
+      context "is not a triangle" do
+        subject(:triangle) {@invalid_scalene}
+          it 'should return true' do
+            expect(triangle.is_triangle?).to be(false) 
+        end
+      end
+
     end
 
     describe '#show' do
-        subject(:triangle) {@triangle_scalene}
+
+        subject(:valid_scalene) {@valid_scalene}
+        subject(:valid_isosceles) {@valid_isosceles}
         it 'should show edges' do
-            expect(triangle.show).to eq('1, 2, 3')
+            expect(valid_scalene.show).to eq('3, 7, 9')
+            expect(valid_isosceles.show).to eq('6, 6, 7')
         end
     end
 
     describe '#type' do
-        subject(:triangle_scalene) {@triangle_scalene}
-        subject(:triangle_isosceles) {@triangle_isosceles}
+        subject(:valid_scalene) {@valid_scalene}
+        subject(:valid_isosceles) {@valid_isosceles}
+        subject(:valid_equilateral) {@valid_equilateral}
 
         it 'should return triangle type' do
-            expect(triangle_scalene.type).to eq("Scalene")
-            expect(triangle_isosceles.type).to eq("Isosceles")
+            expect(valid_scalene.type).to eq("Scalene")
+            expect(valid_isosceles.type).to eq("Isosceles")
+            expect(valid_equilateral.type).to eq("Equilateral")
         end
     end
 
     describe '#is_equilateral' do
-        subject(:triangle_scalene) {@triangle_scalene}
-        subject(:triangle_isosceles) {@triangle_isosceles}
-        subject(:triangle_equi) {@triangle_equi}
+        subject(:valid_scalene) {@valid_scalene}
+        subject(:valid_isosceles) {@valid_isosceles}
+        subject(:valid_equilateral) {@valid_equilateral}
 
         it 'should return true for triangle_equi, otherwhise false' do
-            expect(triangle_scalene.is_equilateral?).to be(false)
-            expect(triangle_isosceles.is_equilateral?).to be(false)
-            expect(triangle_equi.is_equilateral?).to be(true)
+            expect(valid_scalene.is_equilateral?).to be(false)
+            expect(valid_isosceles.is_equilateral?).to be(false)
+            expect(valid_equilateral.is_equilateral?).to be(true)
         end
     end
 
     describe '#is_isosceles' do
-        subject(:triangle_scalene) {@triangle_scalene}
-        subject(:triangle_isosceles) {@triangle_isosceles}
-        subject(:triangle_equi) {@triangle_equi}
+        subject(:valid_scalene) {@valid_scalene}
+        subject(:valid_isosceles) {@valid_isosceles}
+        subject(:valid_equilateral) {@valid_equilateral}
 
         it 'should return true for equi and isosceles otherwhise false' do
-            expect(triangle_scalene.is_isosceles?).to be(false)
-            expect(triangle_isosceles.is_isosceles?).to be(true)
-            expect(triangle_equi.is_isosceles?).to be(true)
+            expect(valid_scalene.is_isosceles?).to be(false)
+            expect(valid_isosceles.is_isosceles?).to be(true)
+            expect(valid_equilateral.is_isosceles?).to be(true)
         end
     end
 end
